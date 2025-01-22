d1634b4d9afc14da6497b4aa4dc99323f1762ada
bneveret <eve20003@byui.edu>
2025-01-22 15:19:37 -0700

    fixed index route

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 
 const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('filename goes here').find();
+  const result = await mongodb.getDb().db().collection('user').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const indexController = require('../controllers');
+router.get('/', indexController.getData);
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
