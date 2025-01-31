88099d2373136c8cd85278ca9857c590913c6d1f
bneveret <eve20003@byui.edu>
2025-01-30 17:20:50 -0700

    front end compatible

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,23 +2,32 @@ const mongodb = require('../db/connect');
 const {ObjectId} = require('mongodb');
 
 const getContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 };
 
 const getContacts = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 };
 
 const createContact = async (req, res) => {
+  try{
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -36,9 +45,13 @@ const createContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
+} catch (err) {
+  res.status(500).json({message: err.message});
+}
 };
 
 const updateContact = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   const contact = {
     firstName: req.body.firstName,
@@ -58,9 +71,13 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
+} catch (err) {
+  res.status(500).json({message: err.message});
+}
 };
 
 const deleteContact = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   const response = await mongodb
     .getDb()
@@ -73,6 +90,9 @@ const deleteContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
+} catch (err) {
+  res.status(500).json({message: err.message});
+}
 };
 
 module.exports = {
